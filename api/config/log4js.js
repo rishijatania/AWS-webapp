d61c94619f4bc0759bb99c4c6b96deea1e55d781
rishijatania <jatania.r@husky.neu.edu>
2020-03-17 22:12:25 -0400

    Adding cloudwatch changes -12.1

--- api/config/log4js.js
+++ api/config/log4js.js
@@ -1,26 +1,49 @@
-const log4js = require('log4js');
-const logger = log4js.getLogger();
+var appRoot = require('app-root-path');
+var winston = require('winston');
 
-log4js.configure({
-    appenders: {
-        out: {
-            type: 'stdout'
-        },
-        app: {
-            type: 'file',
-            filename: '/home/ubuntu/webapp-log4s.log',
-            maxLogSize: 10485760,
-            backups: 1,
-            compress: true
-        }
+// define the custom settings for each transport (file, console)
+var options = {
+    file: {
+        level: 'info',
+        filename: `${appRoot}/app.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: true,
     },
-    categories: {
-        default: {
-            appenders: ['out', 'app'],
-            level: 'debug'
-        }
-    }
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+    },
+};
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(
+        info => `[${info.timestamp}] :: [${info.level}] :: ${info.message}`
+    ),
+);
+
+// instantiate a new Winston Logger with the settings defined above
+var logger = new winston.createLogger({
+    format : logFormat,
+    transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false, // do not exit on handled exceptions
 });
 
-logger.debug('Logger Level On : ');
+// create a stream object with a 'write' function that will be used by `morgan`
+logger.stream = {
+    write: function(message, encoding) {
+        // use the 'info' log level so the output will be picked up by both transports (file and console)
+        logger.info(message);
+    },
+};
+
 module.exports = logger;
